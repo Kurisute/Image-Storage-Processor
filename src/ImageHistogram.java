3b3e28fca1ad6e5bfefcf2f46197ba9d3546fa5c
Yichen Xie <kurisute.xie@gmail.com>
2022-12-15 04:12:36 -0500

    Add histogram class

---
+++ src/ImageHistogram.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+public class ImageHistogram {
+	static {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
+	
+	public static Mat computeHistogram(Mat frame, boolean gray){
+		// split the frames in multiple images
+		List<Mat> images = new ArrayList<Mat>();
+		Core.split(frame, images);
+		
+		// set the number of bins at 256
+		MatOfInt histSize = new MatOfInt(256);
+		// only one channel
+		MatOfInt channels = new MatOfInt(0);
+		// set the ranges
+		MatOfFloat histRange = new MatOfFloat(0, 256);
+		
+		// compute the histograms for the B, G and R components
+		Mat hist_b = new Mat();
+		Mat hist_g = new Mat();
+		Mat hist_r = new Mat();
+		
+		// B component or gray image
+		Imgproc.calcHist(images.subList(0, 1), channels, new Mat(), hist_b, histSize, histRange, false);
+		
+		// G and R components (if the image is not in gray scale)
+		if (!gray)
+		{
+			Imgproc.calcHist(images.subList(1, 2), channels, new Mat(), hist_g, histSize, histRange, false);
+			Imgproc.calcHist(images.subList(2, 3), channels, new Mat(), hist_r, histSize, histRange, false);
+		}
+		
+		// draw the histogram
+		int hist_w = 200; // width of the histogram image
+		int hist_h = 200; // height of the histogram image
+		int bin_w = (int) Math.round(hist_w / histSize.get(0, 0)[0]);
+		
+		Mat histImage = new Mat(hist_h, hist_w, CvType.CV_8UC3, new Scalar(0, 0, 0));
+		// normalize the result to [0, histImage.rows()]
+		Core.normalize(hist_b, hist_b, 0, histImage.rows(), Core.NORM_MINMAX, -1, new Mat());
+		
+		// for G and R components
+		if (!gray)
+		{
+			Core.normalize(hist_g, hist_g, 0, histImage.rows(), Core.NORM_MINMAX, -1, new Mat());
+			Core.normalize(hist_r, hist_r, 0, histImage.rows(), Core.NORM_MINMAX, -1, new Mat());
+		}
+		
+		// effectively draw the histogram(s)
+		for (int i = 1; i < histSize.get(0, 0)[0]; i++)
+		{
+			// B component or gray image
+			Imgproc.line(histImage, new Point(bin_w * (i - 1), hist_h - Math.round(hist_b.get(i - 1, 0)[0])),
+					new Point(bin_w * (i), hist_h - Math.round(hist_b.get(i, 0)[0])), new Scalar(255, 0, 0), 2, 8, 0);
+			// G and R components (if the image is not in gray scale)
+			if (!gray)
+			{
+				Imgproc.line(histImage, new Point(bin_w * (i - 1), hist_h - Math.round(hist_g.get(i - 1, 0)[0])),
+						new Point(bin_w * (i), hist_h - Math.round(hist_g.get(i, 0)[0])), new Scalar(0, 255, 0), 2, 8,
+						0);
+				Imgproc.line(histImage, new Point(bin_w * (i - 1), hist_h - Math.round(hist_r.get(i - 1, 0)[0])),
+						new Point(bin_w * (i), hist_h - Math.round(hist_r.get(i, 0)[0])), new Scalar(0, 0, 255), 2, 8,
+						0);
+			}
+		}
+		
+		return histImage;
+	}
+	
+	// Unit Test
+	public static void main(String[] args) {
+		String envRootDir = System.getProperty("user.dir");
+		String sampleDir = envRootDir + "\\sample";
+		Mat testimg = Imgcodecs.imread(sampleDir + "\\fruit.jpg");
+		Mat histimg = ImageHistogram.computeHistogram(testimg, false);
+		Imgcodecs.imwrite(sampleDir + "\\fruit_hist.jpg", histimg);
+	}
+
+}
